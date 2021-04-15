e985a07e808690008460c99c940ed5913202acd5
Virginia Herrera <virisabelherreraherrera@hotmail.com>
2021-04-16 00:45:30 +0200

    About us

---
+++ src/components/Navbar/AboutUs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './AboutUs.css';
+
+const AboutUs = props => (
+    <div id="text">
+        <h2 className="title">{props.title}</h2>
+        <h3 className="subtitle">{props.subtitle}</h3>
+        <p className="paragraf">{props.p}</p>
+
+    </div>
+
+)
+
+export default AboutUs;
