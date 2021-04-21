0a913d6f098a7601704fb45c0a49dcf70a34c6df
Manuel Marque <manuelmarq05@gmail.com>
2021-04-21 16:23:49 +0200

    modification

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import ListStationTest from "./components/ListStationTest";
+import ReactAudioPlayer from 'react-audio-player';
+
 
 
 /*--------------Discover------------------*/
@@ -8,6 +10,8 @@ import ListStationTest from "./components/ListStationTest";
 function App() {
   const [text] = useState("world from stations");
   const [stations, setStations] = useState("");
+  //let [valueRadio, setValueRadio] = useState("");
+  let setValueRadio = stations[Math.floor(Math.random() * stations.length)]
 
   const getData = () => {
     fetch("https://de1.api.radio-browser.info/json/stations")
@@ -28,17 +32,41 @@ function App() {
 
   return (
     <div className="App">
-      {/*{console.log(stations)}*/}
+      {console.log(stations)}
+      {console.log(setValueRadio)}
+      {/*{console.log(valueRadio)}*/}
       {/*<h3>{names[Math.floor(Math.random() * names.length)]}</h3><br />*/}
       <ListStationTest />
       <h1>{text}</h1>
       <h2>Station dinamic</h2>
-      <audio controls autoplay name="media">
+      {/*<audio controls autoplay name="media">
         <source src={stations[Math.floor(Math.random() * stations.length)]} type="audio/mpeg"></source>
       </audio>
+      <img alt="img"src={mapStation.favicon} />
+      <a className='nav-radio' href="#">{mapStation.name}</a>*/}
+      
+         {/*{
+          stations.map(mapStation => 
+    
+    <ul className='RadioList'>{<img className="img" src={mapStation.favicon}/> }
+    <a className='nav-radio' href="#" onClick={() => setValueRadio(mapStation.url)}  >{mapStation.name}</a>
+
+    </ul>
+
+    )}*/}
       <br />
       {/*<Map />*/}
-      <button onClick={getData}>Give me radio</button>{" "}
+      <button onClick={getData}>Give me radio</button>
+      <div className="controls">
+    
+    <ReactAudioPlayer
+      src={setValueRadio}
+      autoPlay
+      controls
+    />
+    
+    
+    </div>
     </div>
   );
 }
