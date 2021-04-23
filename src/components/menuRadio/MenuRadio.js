bb706f74bbf5f742d7710410ca2eee43202aea96
Alopmel <78558722+Alopmel@users.noreply.github.com>
2021-04-23 12:47:54 +0200

    PopUp

---
+++ src/components/menuRadio/MenuRadio.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './menuRadio.css'
+
+function MenuRadio(props){
+
+
+  /* Back-Next
+  
+  useEffect(() => {
+    setNextRadioIndex(() => {
+      if (currentRadioIndex + 1 > valueRadio.length - 1) {
+        return 0;
+      } else {
+        return currentRadioIndex + 1;
+      }
+    });
+  }, [currentRadioIndex]);*/
+
+    return (props.trigger) ? (
+
+         
+  <div className="container-popUp"> 
+      
+        <button className="btn-cl-pop" onClick={()=> props.setBottomPopUp(false)}>X</button>
+      
+           
+      
+      {
+          
+          props.countryRadio.filter((country) => country.codec === "MP3").sort((a,b) => b.votes - a.votes ).slice(0,10).map((countrys)=> 
+           <div className="container-all-radios mov-radios"> 
+          {<img className="img" src={countrys.favicon}/> }
+          <a  className='a-radio' onClick={() => props.getRadio(countrys.url_resolved)}>
+          {countrys.name}
+          </a>
+          <button className="button-favorites">+</button>          
+          </div>
+          
+         
+          )}
+    
+      
+  </div> 
+ 
+ 
+    ) : "";
+
+
+
+}
+
+export default MenuRadio;
---
+++ src/components/player/Player.js
@@ -0,0 +1,32 @@
+import React, {useState, useRef, useEffect} from 'react'
+import Controls from './Controls';
+import './player.css'
+
+
+function Player(props) {
+  const audioEl = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const [isCurrentRadio, setIsCurrentRadio] = useState(isPlaying);
+
+
+ 
+  return (
+      <div className="c-player">
+          <audio src={props.valueRadio} autoPlay  muted={isMuted} ref={audioEl}></audio>
+          <Controls 
+          setIsMuted={setIsMuted} 
+          isMuted={isMuted} 
+          setIsMuted={setIsMuted}
+          audioEl={audioEl} 
+          isPlaying={isPlaying} 
+          setIsPlaying={setIsPlaying}
+          isCurrentRadio={isCurrentRadio}
+          setIsCurrentRadio={setIsCurrentRadio}
+          />
+          
+      </div>
+  )
+}
+
+export default Player;
