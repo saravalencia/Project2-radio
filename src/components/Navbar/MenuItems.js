ac7a72df9abb553904bde9e7b65d314de3c69762
Sara Valencia <sara.valencia.m@gmail.com>
2021-05-06 23:33:58 +0200

    small changes

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -8,17 +8,17 @@
         cName: 'nav-links',
     },
     
-    {   
-        icon: <i className="fas fa-grin-tongue"></i>,
-        title: 'About Us',
-        url:"/aboutUs",
-        cName: 'nav-links',
-    },
+    
     {   
         icon: <i className="fas fa-heart"></i>,
         title: 'Favorites',
         url:"/favorites",
         cName: 'nav-links',
     },
-    
+    {   
+        icon: <i className="fas fa-grin-tongue"></i>,
+        title: 'About Us',
+        url:"/aboutUs",
+        cName: 'nav-links',
+    },
 ]
